03ae52b12d1332a8960bba0dbabd8b2edfc1883a
minvenanne <ariise21@student.aau.dk>
2021-11-22 15:40:03 +0100

    Setup booking page
    Added images and sorted the booking by item

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, request  
+from flask import Flask, render_template, redirect, url_for, request
+
 
 # we use url_for to get urls for every html page we route to. this takes the name of an html file and 
 # gives us a url for this
@@ -18,7 +19,7 @@ class Booking:
 # hardcode bookings using our Booking class
 booking1 = Booking("Drill", "Victor Jensen", "19-11-2021", "20-11-2021", "villerdk@hotmail.com")
 booking2 = Booking("Ladder", "Trine Andersen", "19-11-2021", "23-11-2021", "tandersen@example.com")
-booking3 = Booking("Extra chair", "Emil Bo", "19-12-2021", "30-12-2021", "e18bo@example.com")
+booking3 = Booking("Chairs", "Emil Bo", "19-12-2021", "30-12-2021", "e18bo@example.com")
 
 bookingList = [booking1, booking2, booking3]
 #when a booking is created the booking is added to bookingList with .append(), so bookingList.append(NameOfBooking)
