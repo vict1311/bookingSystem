69645a8878c10d5becc29661422d5066c4dfbe45
vict1311 <91823836+vict1311@users.noreply.github.com>
2021-11-25 15:30:36 +0100

    Delete_booking
    Created pop-up for deletion on bookings-page that checks list for corresponding entries. Styled pop-ups to be left-aligned.

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 # create Booking class that we can load instances of
 class Booking:
-    def __init__(self, itemName, personName, startDate, endDate, ID):
+    def __init__(self, itemName, fullName, startDate, endDate, email):
         self.itemName = itemName
-        self.personName = personName
+        self.fullName = fullName
         self.startDate = startDate
         self.endDate = endDate
-        self.ID = ID
+        self.email = email
 
 
 # hardcode bookings using our Booking class
@@ -51,6 +51,27 @@ def catalogue():
         bookingList.append(Booking(itemName, fullName, startDate, endDate, email))
         return redirect("/catalogue")
 
-@app.route("/bookings")
+@app.route("/bookings", methods=["GET", "POST"])
 def bookings():
-    return render_template("bookings.html", bookingList = bookingList)
+    if request.method == "GET": 
+        return render_template("bookings.html", bookingList = bookingList)
+        #if the user requests to GET data (get data from resource) we show them our page
+    else:
+        #if the user requests to POST data (send data to resource) we take info from our HTML form and save it as variables
+        itemName = request.form.get("itemName")
+        fullName = request.form.get("fullName")
+        startDate = request.form.get("startDate")
+        endDate = request.form.get("endDate")
+        email = request.form.get("email")
+        
+        # we remove from our mock database by going through every entry in the bookingList
+        for i in bookingList:
+            # we compare every attribute of the i'th object with the value in the submitted form
+            if i.itemName == itemName and i.fullName == fullName and i.startDate == startDate and i.endDate == endDate and i.email == email:
+                # if the above are true, we remove() the entry from the list
+                bookingList.remove(i)
+            #else:
+                # if the entry does not work we show
+             #   return redirect("/contact")
+
+        return redirect("/bookings")
